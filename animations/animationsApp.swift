a3df63f89f180c328cc180da86b79ddf602a29e3
Caglar Cakar <caglar@dijitalgaraj.com>
2022-01-06 21:59:40 +0300

    Initial Commit

---
+++ animations/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  animations
+//
+//  Created by Caglar Cakar on 6.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
